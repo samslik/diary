d54504a50786aa6fa94ff4b61347d4bb5c991cf3
Anna Bochniak <samslik@gmail.com>
2016-04-24 22:01:53 +0200

    Initial commit

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,35 @@
+class EntriesController < ApplicationController
+	def index
+		@entries = Entry.all
+	end
+	
+	def show
+		@entry = Entry.find(params["id"])
+	end
+	
+	def new
+		@entry = Entry.new
+	end
+	
+	def create
+	  entry_params = params["entry"].permit("title", "contents")
+	  entry = Entry.create(entry_params)
+	  redirect_to(entry_path(entry))
+	end
+
+	def edit
+	  @entry = Entry.find(params["id"])
+	end
+	def update
+	  entry_params = params["entry"].permit("title", "contents")
+	  entry = Entry.find(params["id"])
+	  entry.update(entry_params)
+	  redirect_to(entry_path(entry))
+   end
+   def destroy
+	  @entry = Entry.find(params[:id])
+      @entry.destroy
+ 
+  redirect_to entries_path
+   end
+end
